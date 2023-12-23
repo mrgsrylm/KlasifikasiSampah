873593071d8eb0594e09fbb39acdcd76c402c200
Agus S. Mubarok <gusrylmubarok@gmail.com>
2023-12-24 00:54:48 +0700

    Update datetime_converters.dart

--- android/sistem_klasifikasi_sampah_otomatis/lib/service/datetime_converters.dart
+++ android/sistem_klasifikasi_sampah_otomatis/lib/service/datetime_converters.dart
@@ -1,9 +1,10 @@
+import 'package:intl/intl.dart';
+
 class DatetimeConverters {
   static final DatetimeConverters constants = ImageConverters._();
   factory DatetimeConverters() => constants;
   ImageConverters._();
   String convertToAsiaJakarta(int unixTimestampMillis) {
-
     DateTime dateTimeMillis = DateTime.fromMillisecondsSinceEpoch(unixTimestampMillis);    
     String result = DateFormat.yMd().add_Hms().format(dateTimeMillis.toLocal());
 
